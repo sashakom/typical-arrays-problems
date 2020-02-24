77fd78937e0664b3c32eb27e93d621d064a69a2b
Oleksandr Komar <35637943+sashakom@users.noreply.github.com>
2020-02-24 15:29:07 +0200

    init: start task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let min = array[0];
+      array.forEach((item) => {
+        if (item <= min) {
+          min = item;
+        }
+      });
+      return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let max = array[0];
+      array.forEach((item) => {
+        if (item >= max) {
+          max = item;
+        }
+      });
+      return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+      let sum = 0;
+      let avg = 0;
+      array.forEach((item) => {
+        sum += item;
+      });
+      avg = sum / array.length;
+      return avg;
+  }
 }
